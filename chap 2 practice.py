9d13a0503b4006c46141da1096566fa76dee20d9
Likith <srisailikithpm@gmail.com>
2026-03-20 23:39:54 +0530

    Add Python programs for basic arithmetic operations

---
+++ chap 2 practice.py
@@ -0,0 +1,35 @@
+# Q1.write a python program to add two numbers . 
+a = int(input("enter the value of a"))
+b = int(input("enter the value of b"))
+sum = a+b
+print(sum)
+
+
+# Q2.Write a python program to find remainder when a number is divided by Z .
+a = 35
+b = 45
+ 
+print("the remainder when a is divided by b is:" , a % b )
+
+
+
+# Q3.chek the type of variable assigned using input() function . 
+a = input("enter the value of a ")
+print(type(a))
+
+# Q4.Use comparission operator to find out wether a given variable is greater than 'b' or not . take a=344,b=80.
+a = 34
+b = 80
+print("the statement a is greater than b is :",a>b)
+
+# Q5.write a python program to find an average of two numbers enterd by the user .
+a = int(input("entr the value of a"))
+b = int(input("enter the value of b"))
+print("the average of a and b is :" , (a+b)/2)
+
+
+# Q6.write a python program to calculate the square of  a number enterd by the user .
+a = int(input("enter the value of numer you want to sqare"))
+print("your answer is:",a**2)
+
+
